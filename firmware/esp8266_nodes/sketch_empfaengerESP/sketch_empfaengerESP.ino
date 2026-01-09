9c06238eb4d85e7ba76134bfd9402e05523170f4
p-keminer <p.keminer@gmx.de>
2026-01-09 06:31:52 +0100

    Update sketch_empfaengerESP.ino

--- firmware/esp8266_nodes/sketch_empfaengerESP/sketch_empfaengerESP.ino
+++ firmware/esp8266_nodes/sketch_empfaengerESP/sketch_empfaengerESP.ino
@@ -1,14 +1,14 @@
     // --- Blynk Konfiguration ---
 
-#define BLYNK_PRINT Serial					  // Debug-Ausgabe über Seriellen Monitor
+#define BLYNK_PRINT Serial					  	// Debug-Ausgabe über Seriellen Monitor
 #define BLYNK_TEMPLATE_ID ""					// <- Blynk Template ID hier eintragen "x"
-#define BLYNK_TEMPLATE_NAME ""				// <- Blynk Template Name hier eintragen "x"
+#define BLYNK_TEMPLATE_NAME ""					// <- Blynk Template Name hier eintragen "x"
 #define BLYNK_AUTH_TOKEN  ""					// <- Blynk Auth Token hier eintragen "x"
 
 
-#include <ESP8266WiFi.h>							// einbinden der Bibliotheken ESP8266 WLAN
-#include <WiFiUdp.h>							  	//                            UDP-Kommunikation
-#include <BlynkSimpleEsp8266.h>				//                            Blynk Library für ESP8266
+#include <ESP8266WiFi.h>						// einbinden der Bibliotheken ESP8266 WLAN
+#include <WiFiUdp.h>							//                            UDP-Kommunikation
+#include <BlynkSimpleEsp8266.h>					//                            Blynk Library für ESP8266
 
 
 
@@ -17,7 +17,7 @@
 #define LED_WIFI D3							
 
 
-#define BUZZER1_PIN D5						// definieren der Buzzer Pins
+#define BUZZER1_PIN D5							// definieren der Buzzer Pins
 #define BUZZER2_PIN D6							
 
 
@@ -32,18 +32,18 @@ const char* password = "";									// <- WLAN Passwort hier eintragen "xyz"
 
 char auth[] = BLYNK_AUTH_TOKEN;					
 
-WiFiUDP udp;										            // UDP Objekt erstellen
+WiFiUDP udp;										// UDP Objekt erstellen
 const unsigned int localPort = 4210;				// Lokaler Port für UDP
 
     // --- Alarmvariablen ---
 
-bool alarmEmpfangen = false;			          // Statusvariablen für Empfang 			  
+bool alarmEmpfangen = false;			          	// Statusvariablen für Empfang & Aktivierung		  
 bool alarmAn = false;								      
 
 
     // --- LED Variablen ---
 
-bool ledStatus = false;							      	// LED Toggle Variablen
+bool ledStatus = false;							    // LED Toggle Variablen
 unsigned long toggleZuletzt = 0;					
 unsigned long toggleInterval = 200;				
 unsigned long blinkZuletzt = 0;					  
@@ -51,7 +51,7 @@ unsigned long blinkInterval = 500;
 
     // --- Taster Variablen ---
 
-bool tasterZuletzt = HIGH;									// Taster Toggle/Status Variablen
+bool tasterZuletzt = HIGH;							// Taster Toggle/Status Variablen
 bool tasterJetzt = HIGH;							
 
 
@@ -60,16 +60,16 @@ bool tasterJetzt = HIGH;
     // --- Funktionen UDP  ---
 
 /* prueft auf eingehende Nachrichten, liest aus,
-wandelt um & aktiviert funktionellen Alarm*/
+wandelt um & aktiviert Alarm*/
 
 void pruefeUDP() {
 	int packetSize = udp.parsePacket();						// Prüfe auf eingehendes Paket
 	if (packetSize) {
 		char buffer[255];									
-		int len = udp.read(buffer, 254);					  // Nachricht einlesen
-		if (len > 0) buffer[len] = 0;						    // Null-Terminierung
-		String msg = String(buffer);						    // In String umwandeln
-		msg.trim();										              // Leerzeichen & Zeilenumbrueche entfernen (sauberer Vergleich)
+		int len = udp.read(buffer, 254);					// Nachricht einlesen
+		if (len > 0) buffer[len] = 0;						// Null-Terminierung
+		String msg = String(buffer);						// In String umwandeln
+		msg.trim();										    // Leerzeichen & Zeilenumbrueche entfernen (sauberer Vergleich)
 
 		if (msg == "ALARM_ON") {							
 			alarmEmpfangen = true;
@@ -103,31 +103,31 @@ void tasterSetztAlarm() {
 	tasterJetzt = digitalRead(TASTER_PIN);				
 
 	
-  if (tasterZuletzt == HIGH && tasterJetzt == LOW) {    // Taster gedrueckt ?
+  if (tasterZuletzt == HIGH && tasterJetzt == LOW) {    	// Taster gedrueckt ?
 		alarmAn = !alarmAn;								
-		alarmEmpfangen = false;							   // Sender-Signal ignorieren
-		Serial.println(alarmAn ? "Alarm manuell aktiviert" : "Alarm manuell deaktiviert");   // Debugging Ausgabe
+		alarmEmpfangen = false;							   	// Sender-Signal ignorieren
+		Serial.println(alarmAn ? "Alarm manuell aktiviert" : "Alarm manuell deaktiviert");   	// Debugging Ausgabe
 	}
-	tasterZuletzt = tasterJetzt;						 // Status speichern
+	tasterZuletzt = tasterJetzt;						 	// Status speichern
 }
 
 
     // --- Blynk Funktion ---
 
-/* ermöglicht manuelles de- & aktivieren 
+/* ermoeglicht manuelles de- & aktivieren 
 des Alarms per App oder Dashboard*/
 
 BLYNK_WRITE(V0) {										
-	int value = param.asInt();							        // Wert auslesen & speichern
+	int value = param.asInt();							       	// Wert auslesen & speichern
 
-	if (value == 1) {										            // Alarm aktivieren
+	if (value == 1) {										    // Alarm aktivieren
 		alarmAn = true;
 		alarmEmpfangen = false;
-		Serial.println("Alarm über Blynk EIN");       			// Debugging Ausgabe
+		Serial.println("Alarm über Blynk EIN");       					// Debugging Ausgabe
 	} else {												                
-		alarmAn = false;															// Alarm deaktivieren
+		alarmAn = false;										// Alarm deaktivieren
 		alarmEmpfangen = false;
-		Serial.println("Alarm über Blynk AUS");       			// Debugging Ausgabe
+		Serial.println("Alarm über Blynk AUS");       					// Debugging Ausgabe
 	}
 }
 
@@ -137,19 +137,19 @@ BLYNK_WRITE(V0) {
 /* schaltet LEDs und Buzzer togglend an*/
 
 void updateAlarm() {
-	if(alarmAn){											    										// wenn Alarm aktiviert 
+	if(alarmAn){											    		// wenn Alarm aktiviert 
 		
 		if (millis() - toggleZuletzt >= toggleInterval) {				 
-			ledStatus = !ledStatus;																// wenn LED umschaltzeit >= intervall
-			digitalWrite(LEDR_PIN, ledStatus ? HIGH : LOW);				// LED & Buzzer togglen
+			ledStatus = !ledStatus;										// wenn LED umschaltzeit >= intervall,
+			digitalWrite(LEDR_PIN, ledStatus ? HIGH : LOW);				// LED & Buzzer togglen (status = !status)
 			digitalWrite(BUZZER2_PIN, ledStatus ? HIGH : LOW);
 			digitalWrite(LEDY_PIN, !ledStatus ? HIGH : LOW);
 			digitalWrite(BUZZER1_PIN, ledStatus ? HIGH : LOW);
-			toggleZuletzt = millis();															// speichere umschaltzeit
+			toggleZuletzt = millis();									// speichere umschaltzeit
 		}
 	} else {												
-		digitalWrite(BUZZER1_PIN, LOW);     										// wenn Alarm deaktiviert 
-		digitalWrite(BUZZER2_PIN, LOW);													// deaktiviere alles
+		digitalWrite(BUZZER1_PIN, LOW);     							// wenn Alarm deaktiviert, 
+		digitalWrite(BUZZER2_PIN, LOW);									// deaktiviere alles
 		digitalWrite(LEDR_PIN, LOW);
 		digitalWrite(LEDY_PIN, LOW);
 	}
@@ -158,7 +158,7 @@ void updateAlarm() {
 
 		// ---Funktion WLAN LED ---
 	
-/*WLAN Status anzeige*/
+/*WLAN Status Anzeige*/
 
 void updateLedWLAN() {
 	if (WiFi.status() == WL_CONNECTED) {										
@@ -173,7 +173,7 @@ void updateLedWLAN() {
 
 void setup() {
 	
-	Serial.begin(9600);											// Seriellen Monitor starten
+	Serial.begin(9600);									// Seriellen Monitor starten
 
 	pinMode(LED_WIFI, OUTPUT);							// initialisieren LED, Buzzer und Taster Pin/s 
 	pinMode(LEDR_PIN, OUTPUT);
@@ -188,15 +188,15 @@ void setup() {
 	digitalWrite(BUZZER1_PIN, LOW);
 	digitalWrite(BUZZER2_PIN, LOW);
 
-	WiFi.begin(ssid, password);							// WLAN verbinden
-	Serial.print("Verbinde mit WLAN");
+	WiFi.begin(ssid, password);												// WLAN verbinden
+	Serial.print("Verbinde mit WLAN");											// Debugging Ausgabe
 
 	
-	udp.begin(localPort);										// UDP starten
+	udp.begin(localPort);													// UDP starten
 	Serial.println("UDP-Empfang gestartet auf Port " + String(localPort));
 
 	
-	Blynk.begin(auth, ssid, password);			// Blynk initialisieren
+	Blynk.begin(auth, ssid, password);										// Blynk initialisieren
 }
 
 
@@ -204,8 +204,8 @@ void setup() {
 
 void loop() {
 	
-	if (WiFi.status() != WL_CONNECTED) {						// WLAN prüfen
-		WiFi.reconnect();															// erneut verbinden
+	if (WiFi.status() != WL_CONNECTED) {									// WLAN pruefen
+		WiFi.reconnect();													// erneut verbinden
 	}
 
 	updateLedWLAN();											
@@ -216,5 +216,5 @@ void loop() {
 	updateAlarm();												
 
 	
-	Blynk.virtualWrite(V1, alarmAn ? 255 : 0); 			// Blynk input aktualisieren
+	Blynk.virtualWrite(V1, alarmAn ? 255 : 0); 								// Blynk input aktualisieren
 }
